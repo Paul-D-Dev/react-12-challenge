67c1c20cc8d440c77c70023a80d16118a495124a
Paul DAO <dao.van.paul@gmail.com>
2020-03-30 12:27:32 +0200

    Updates

---
+++ precache-manifest.373fae1fd0e055af647095f98af5d5ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d3a6e603032624961a2aecd2b61ff5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed3b6ae70c0639d26fbe",
+    "url": "/static/css/main.656f0744.chunk.css"
+  },
+  {
+    "revision": "7819b135a6dbd26e6b09",
+    "url": "/static/js/2.36d204e7.chunk.js"
+  },
+  {
+    "revision": "ed3b6ae70c0639d26fbe",
+    "url": "/static/js/main.432a7657.chunk.js"
+  },
+  {
+    "revision": "a72d5ff9ddec5914d20c",
+    "url": "/static/js/runtime-main.6b85e05f.js"
+  }
+]);
